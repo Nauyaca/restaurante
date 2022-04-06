eabdcc9c4c03c939748409a1a2d31d50b6dfd73d
nauyaca <tiburon-nahu@outlook.com>
2022-04-06 18:47:07 -0500

    avatar

--- restaurante/app/screens/Account/UserLogged.js
+++ restaurante/app/screens/Account/UserLogged.js
@@ -1,14 +1,56 @@
-import React from 'react'
-import {View, Text, Button } from 'react-native'
+import React, {useState, useRef, useEffect} from 'react'
+import {View, Text, StyleSheet } from 'react-native'
 import firebase from 'firebase'
+import Toast from 'react-native-toast-message'
+import {Button} from 'react-native-elements'
+import InfoUser from '../../components/Account/InfoUser'
+
+
 
 export default function UserLogged(){
+    const [userInfo, setUserInfo] = useState(null)
+    const toastRef = useRef()
+    useEffect(()=>{
+        (async()=>{
+            const user = await firebase.auth().
+            currentUser
+            setUserInfo(user)
+        })()
+    }, [])
     return(
-        <View>
+        <View style={styles.viewUserInfo}>
+            {userInfo&&<InfoUser userInfo={userInfo}/>}
             <Text>
-                UserLogged
+             AccountOptions
             </Text>
-            <Button title= 'Cerrar sesion' onPress={()=>firebase.auth().signOut()}/>
+            <Button 
+                title= 'Cerrar sesion' 
+                buttonStyle={styles.btnCloseSession}
+                titleStyle={styles.btnCloseSessionText}
+                onPress={()=>firebase.auth().signOut()}
+            />
+            <Toast ref= {toastRef}/>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+   viewUserInfo:{
+       minHeight: '100%',
+       backgroundColor: '#f2f2f2'
+   },
+   btnCloseSession:{
+       marginTop: 30,
+       borderRadius: 0,
+       backgroundColor: '#00a680',
+       borderTopWidth: 1,
+       borderTopColor: '#e3e3e3',
+       borderBottomWidth: 1,
+       borderBottomColor: '#e3e3e3',
+       paddingTop: 10,
+       paddingBottom:10
+   },
+   btnCloseSessionText:{
+     color:'#fff'
+   }
+})

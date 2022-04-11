4e6d65c1b7cea2b058e2224b0ee822516aca63f0
Nauyaca <tiburon-nahu@outlook.com>
2022-04-10 23:59:47 -0500

    Modal

--- restaurante/app/components/Account/AccountOptions.js
+++ restaurante/app/components/Account/AccountOptions.js
@@ -1,12 +1,32 @@
-import React from "react"
+import React, {useState} from "react"
 import {StyleSheet, View, Text } from "react-native"
 import {ListItem, Icon} from 'react-native-elements'
+import Modal from '../Modal'
 
 export default function AccountOptions(props){
     const {userInfo, toastRef} = props
+    const [showModal, setShowModal] = useState (false)
+    const [renderComponent, setRenderComponent] = useState(null)
+    
     const selectedComponent = (key) =>{
-        console.log('click')
-        console.log(key)
+       switch(key){
+           case 'displayName':
+               setRenderComponent(<Text>Cambiando nombre y apellido</Text>)
+               setShowModal(true)
+               break
+            case 'displayEmail':
+            setRenderComponent(<Text>Cambiando email</Text>)
+            setShowModal(true)
+               break
+            case 'displayPassword':
+            setRenderComponent(<Text>Cambiando contraseña</Text>)
+            setShowModal(true)
+                break
+                default:
+                    setRenderComponent(null)
+                    setShowModal(false)
+                    break
+       }
     }
     const menuOptions = generateOptions(selectedComponent)
 
@@ -20,6 +40,11 @@ export default function AccountOptions(props){
                     </ListItem.Content>
                 </ListItem>
             ))}
+            {renderComponent && (
+            <Modal isVisible={showModal} setIsVisible={setShowModal}>
+                {renderComponent}
+            </Modal>
+            )}
         </View>
     )
 }
